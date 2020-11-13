4cfd7795747196206c092e1b19fe6a173f13c4a8
Maria Jose Mendez <majomendez88mena@gmail.com>
2020-11-12 18:09:10 -0600

    First approach

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const product = 'Socks'
+/** Large solution */
+/**  const app = Vue.createApp({
+  data: function(){
+    return {
+      product: 'Sockets'
+    }
+  }
+}) */
+
+const app = Vue.createApp({
+  data(){
+    return {
+      product: 'Socks',
+      description: 'Socks in different colors'
+    }
+  }
+})
